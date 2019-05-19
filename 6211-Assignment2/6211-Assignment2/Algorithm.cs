e882feb02744b8331da863534dc9f95941fac5d9
hamishwaddy <hamishwaddy@gmail.com>
2019-05-19 13:19:22 +1200

    q1 & 2 setup but not working

---
+++ 6211-Assignment2/6211-Assignment2/Algorithm.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6211_Assignment2
+{
+    public class Algorithm
+    {
+        static Random random = new Random();
+        public static int[] numbers = new int[50];
+        public static List<int> maxNumbersDescending = new List<int>();
+        
+
+        public static int[] CreateDataset(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(10, 31);
+            }
+            return arr;
+        }
+
+        public static void Display(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+                if ((i + 1) % 25 == 0)
+                    Console.WriteLine();
+            }
+        }
+
+        public static void DisplayList(List<int> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i] + " ");
+                if ((i + 1) % 25 == 0)
+                    Console.WriteLine();
+            }
+        }
+
+        // Question 1
+
+        public static int FindMaximum(int[] arr)
+        {
+            int currentLargest = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                // Search array using linear search
+                // Find max number in array
+                if (arr[i] > currentLargest)
+                {
+                    currentLargest = arr[i];
+                }
+            }
+            // Add max number to list
+            maxNumbersDescending.Add(currentLargest); // This method adds highest number twice. How do I then delete this number from array (or re-assign its value to -1) so next highest number will be found next (and added to maxNumbersDescending list?
+            return currentLargest;
+
+            /*
+            // Display n largest numbers
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+                if ((i + 1) % 25 == 0)
+                    Console.WriteLine();
+            }
+            */
+        }
+
+        // Question 2
+
+        public static void NumOccurenceSearch(int[] arr, int value1, int value2)
+        {
+            int[] numbersClone2 = new int[Algorithm.numbers.Length];
+            Array.Copy(numbers, numbersClone2, numbers.Length);
+            
+            // Display the elements of the array.
+            Console.WriteLine("The array contains the following values:");
+            for (int i = numbersClone2.GetLowerBound(0); i <= numbersClone2.GetUpperBound(0); i++)
+            Console.WriteLine("   [{0,2}]: {1}", i, numbersClone2[i]);
+
+            // Search for the first occurrence of the duplicated value.
+            int searchNum = value1;
+            int index = Array.IndexOf(arr, searchNum);
+            Console.WriteLine("The first occurrence of \"{0}\" is at index {1}.", searchNum, index);
+
+            // Search for the user chosen occurrence ('value2') of the value.
+            searchNum= value1;
+            int searchOccurence = value2;
+            index = Array.IndexOf(arr[], searchNum);
+            Console.WriteLine("The item {0} has occurence {1} at index {2}.", value1, value2, index);
+        }
+
+
+        /*
+            foreach (int number in arr)
+            {
+                int count;
+                dict.TryGetValue(number, out count);
+                count++;
+                // Replace the entry if it already exists
+                dict[number] = count;
+            }
+            int chosenNumber = 0, ocurrences = 0;
+            foreach (var pair in dict)
+            {
+                if (pair.Value > ocurrences)
+                {
+                    ocurrences = pair.Value;
+                    chosenNumber = pair.Key;
+                }
+            }
+            Console.WriteLine("The chosen number is {0} and it appears {1} times", chosenNumber, ocurrences);
+            */
+
+
+        //int indexPosition = 0;
+        ////Display the index of the occurence
+        //for (int i = 0; i < arr.Length; i++)
+        //{
+        //    if (arr[i].Equals(value1))
+        //    {
+        //        arr[i] = indexPosition;
+        //        dict.Add(1, indexPosition);
+        //        // need to increment 'key'
+
+        //    }
+        //    else
+        //        Console.WriteLine("Your number was not found in the array");
+        //}
+        //Console.WriteLine("The item {0} has occurance {1} at index: {2}", value1, value2, indexPosition);
+        //foreach (KeyValuePair<int, int> valuePair in dict)
+        //{
+        //    Console.WriteLine("Key: {0}, Value: {1}",
+        //    dict.Keys, dict.Values);
+        //}
+
+        public static void DisplayDictionary(Dictionary<int, int> dict)
+        {
+            foreach (KeyValuePair<int, int> valuePair in dict)
+            {
+                Console.WriteLine("Key: {0}, Value: {1}",
+                dict.Keys, dict.Values);
+            }
+        }
+    }
+
+        
+}
---
+++ 6211-Assignment2/6211-Assignment2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6211_Assignment2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**********************************************************");
+            Console.WriteLine("******************* 6211 ASSIGNMENT 2 ********************");
+            Console.WriteLine("*************** Hamish Wadsworth 9835077 *****************");
+            Console.WriteLine("**********************************************************\n\n");
+
+            // QUESTION 1
+            Console.WriteLine("QUESTION 1\nYou are required to write a console application with an Algorithm class.\nThis class should contain a Numbers array which contains 50 integer elements.These elements are to be generated at random.\nYou are to write a Display method, which will display the contents of this array to the screen.\nAdd the appropriate code to Main, so the numbers are displayed.");
+            Console.WriteLine("The particular challenge for this question is to then add a findMaximum method to your program that will receive two parameters:\n\tan array\n\tan integer(n = number of maximum elements to be found)\nThis method has the job of displaying the n largest elements in the array it receives and displaying it to the screen as shown in the sample program below.\nNOTE:  Do not use the array sort method to complete this challenge.");
+
+            Console.WriteLine("\n\nOriginal Array");
+            Algorithm.CreateDataset(Algorithm.numbers);
+            Algorithm.Display(Algorithm.numbers);
+            int[] numbersClone = new int[Algorithm.numbers.Length];
+            Array.Copy(Algorithm.numbers, numbersClone, Algorithm.numbers.Length);
+            Console.WriteLine("\n\nData clone");
+            Algorithm.Display(numbersClone);
+
+            // testing findmaximum method
+            Algorithm.FindMaximum(numbersClone);
+            Console.WriteLine("Largerst number: {0}", Algorithm.FindMaximum(numbersClone));
+
+            // testing maxNumbersDescending list
+            Console.WriteLine("\n\nmaxNumbersDescending List");
+            Algorithm.DisplayList(Algorithm.maxNumbersDescending);
+            Console.WriteLine("\nMax Numbers List count: {0}", Algorithm.maxNumbersDescending.Count());
+
+            //Console.Write("\nHow many maximum values would you like to find? ");
+            //int n = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"\n\nThe {n} largest elements in array are:");
+            //Algorithm.FindMaximum(numbersClone, n);
+
+            // QUESTION 2
+            int[] numbersClone2 = new int[Algorithm.numbers.Length];
+            Array.Copy(Algorithm.numbers, numbersClone2, Algorithm.numbers.Length);
+            Console.WriteLine("\n\n\n*** QUESTION 2 ***\n\n");
+            Console.WriteLine("Data Clone 2");
+            Algorithm.Display(numbersClone2);
+            Console.Write("\nWhat value do you want to find: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nWhich occurence do you want to find");
+            int b = int.Parse(Console.ReadLine());
+            Algorithm.NumOccurenceSearch(numbersClone2, a, b);
+
+
+            Console.ReadLine();
+        }
+    }
+}
